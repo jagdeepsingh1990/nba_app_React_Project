c8028af50bf0ecd3902d7349efd0a9e17199a576
Jagdeep Singh <jagdeepsingh@Jagdeeps-MBP.hitronhub.home>
2019-07-03 14:39:30 -0500

    card for newslist implented

--- src/components/widgets/NewsSlider/slider.js
+++ src/components/widgets/NewsSlider/slider.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import SliderTemplates from './slider_templates';
 
+import { URL } from '../../../config';
+
 class NewsSlider extends Component {
 
 state = {
     news:[]
 }
 componentWillMount(){   
-    axios.get(`http://localhost:3004/articles?start=${this.props.start}&_end=${this.props.amount}`).then(response =>{
+    axios.get(`${URL}/articles?start=${this.props.start}&_end=${this.props.amount}`).then(response =>{
         this.setState({
             news:response.data
         })

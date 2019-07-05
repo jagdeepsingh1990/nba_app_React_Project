3094f405cf44b1433d4beac63e74c347faed7948
Jagdeep Singh <jagdeepsingh@Jagdeeps-MBP.hitronhub.home>
2019-07-05 15:03:06 -0500

    card implemented to feeds

---
+++ src/components/widgets/CardInfo/cardinfo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock} from '@fortawesome/free-solid-svg-icons';
+import styles from './cardinfo.css'
+
+const CardInfo =(props)=> {
+
+    const teamName = (teams, team) => {
+        let data = teams.find((item)=>{
+            return item.id === team
+        });
+        if(data){
+            return data.name
+        }
+    }
+    return(
+        <div className={styles.cardInfo}>
+            <span className={styles.teamName}>
+                {teamName(props.teams,props.team)}
+            </span>
+            <span className={styles.date}>
+            <FontAwesomeIcon icon={faClock} />
+                {props.date}
+            </span>
+            
+        </div>
+
+
+    )
+}
+export default CardInfo;

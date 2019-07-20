15bc824467d501d1797208d27056f5f94fcc2855
Jagdeep Singh <jagdeepsingh@Jagdeeps-MBP.hitronhub.home>
2019-07-20 14:59:19 -0500

    form Validation on Sign in done

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,57 +1,110 @@
-import React ,{Component} from 'react';
+import React, { Component } from 'react';
 import styles from './signin.css';
 import FormField from '../widgets/FormFields/formFields';
 
 class SignIn extends Component {
     state = {
-        registerError:'',
-        loading:false,
-        formdata:{
-            email:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'email_input',
-                    type:'email',
-                    placeholder:'Enter your email'
+        registerError: '',
+        loading: false,
+        formdata: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
                 },
-                validation:{
-                    required:true,
-                    email:true
+                validation: {
+                    required: true,
+                    email: true
                 },
-                valid:false,
-                touched:false,
-                validationMessage:''
+                valid: false,
+                touched: false,
+                validationMessage: ''
             },
-            password:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'password_input',
-                    type:'password',
-                    placeholder:'Enter your password'
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'password_input',
+                    type: 'password',
+                    placeholder: 'Enter your password'
                 },
-                validation:{
-                    required:true,
-                    email:true
+                validation: {
+                    required: true,
+                    password: true
                 },
-                valid:false,
-                touched:false,
-                validationMessage:''
+                valid: false,
+                touched: false,
+                validationMessage: ''
             }
         }
     }
-    render(){
-        return(
+    updateForm = (element) => {
+        const newFormData = {
+            ...this.state.formdata
+        }
+        const newElement = {
+            ...newFormData[element.id]
+        }
+        newElement.value = element.event.target.value;
+
+        if (element.blur) {
+            let validData = this.validate(newElement);
+            newElement.valid = validData[0];
+            newElement.validationMessage = validData[1];
+        }
+        newElement.touched = element.blur;
+
+        newFormData[element.id] = newElement;
+
+        this.setState({
+            formdata: newFormData
+        })
+    }
+    validate = (element) => {
+        let error = [true, ''];
+       
+
+        if (element.validation.email) {
+            const valid = /\S+@\S+\.\S+/.test(element.value);
+            const message = `${!valid ? 'Must be valid email' : ''}`
+            error = !valid ? [valid, message] : error
+        } 
+
+        if (element.validation.password) {
+            const valid = element.value.length >= 5;
+            const message = `${!valid ? 'Must be greater than 5' : ''}`
+            error = !valid ? [valid, message] : error
+        } 
+
+
+        if (element.validation.required) {
+                const valid = element.value.trim() !== '';
+                const message = `${!valid ? 'This Field is required' : ''}`
+                error = !valid ? [valid, message] : error
+            }
+        return error;
+    }
+    render() {
+        return (
             <div className={styles.logContainer}>
                 <form>
+                    <h2>Register/Login</h2>
                     <FormField
-                    id={'email'}
-                    formdata={this.state.formdata.email}
-                    change={'j'}
+                        id={'email'}
+                        formdata={this.state.formdata.email}
+                        change={(e) => this.updateForm(e)}
+                    />
+
+                    <FormField
+                        id={'password'}
+                        formdata={this.state.formdata.password}
+                        change={(e) => this.updateForm(e)}
                     />
                 </form>
-                
+
             </div>
         )
     }
--- src/components/widgets/FormFields/formFields.js
+++ src/components/widgets/FormFields/formFields.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import styles from './formfields.css';
 
-const FormField =()=>{
-    return(
+const FormField = ({ formdata, change, id }) => {
+
+    const showError=()=>{
+        let errorMessage = null;
+        if(formdata.validation && !formdata.valid){
+            errorMessage=(
+                <div className={styles.labelError}>
+                    {formdata.validationMessage}
+                </div>
+            )
+        }
+        return errorMessage;
+    }
+
+    const renderTemplate = () => {
+        let formtemplate = null;
+
+        switch (formdata.element) {
+            case ('input'):
+                formtemplate = (
+                    <div>
+                        <input
+                        {...formdata.config}
+                        value={formdata.value}
+                        onBlur={(event)=>change({event,id,blur:true})}  
+                        onChange={(event)=>change({event,id,blur:false})}                        
+                        />
+                        {showError()}
+                    </div>
+                )
+                break;
+            default:
+                formtemplate = null;
+        }
+        return formtemplate;
+    }
+    return (
         <div>
-            tempplate input
+            {renderTemplate()}
         </div>
     )
 }
